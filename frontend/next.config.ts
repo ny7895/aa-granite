df9742b05f501e290609660c5573ddbf59122792
ny7895 <noahy78951@gmail.com>
2025-03-28 14:38:18 -0600

    editing for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Recommended for Amplify (includes server files)
+
 };
 
 export default nextConfig;
