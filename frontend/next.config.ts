9cbba40d91e3e76a7c6e103ae1ed04bbe72c07a0
ny7895 <noahy78951@gmail.com>
2025-03-29 15:58:45 -0600

    modify for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,11 @@
 import path from "node:path";
+import { MdOutput } from "react-icons/md";
 
 const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: "standalone",
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
