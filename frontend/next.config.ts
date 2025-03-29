7cc4babf38263ca6e311e09a040efb64721e0620
ny7895 <noahy78951@gmail.com>
2025-03-29 11:19:38 -0600

    modify for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Ensures proper build for Amplify
-  basePath: "/src/app",
   images: {
     unoptimized: true,
   },
