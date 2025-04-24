e9126144e13b2d4d8f73807e6c399e308d505735
ny7895 <noahy78951@gmail.com>
2025-04-24 07:26:09 -0600

    add google ads

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -75,6 +75,20 @@ export default function RootLayout({
             });
           `}
         </Script>
+
+        {/* Google Ads gtag.js */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-614418295"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-adwords" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-614418295');
+          `}
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
